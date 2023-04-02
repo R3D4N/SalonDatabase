13296a4ff19e2cdd5e6a92b01b81a796ec3c9a38
angelo <95056196+R3D4N@users.noreply.github.com>
2023-04-02 18:45:49 -0500

    Completed salon.sh with all process

--- salon.sh
+++ salon.sh
@@ -2,3 +2,67 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo -e "Welcome to My Salon, how can I help you?\n"
+
+MAIN_MENU(){
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+
+  # get all services
+  LIST_SERVICES=$($PSQL "SELECT * FROM services")
+  echo "$LIST_SERVICES" | while read ID BAR SERVICE
+  do
+    echo "$ID) $SERVICE"
+  done
+  
+  # get last service id
+  LAST_ID=$($PSQL "SELECT MAX(service_id) FROM services")
+
+  read SERVICE_ID_SELECTED
+
+  # not found the service
+  if [[ $SERVICE_ID_SELECTED -le $LAST_ID && $SERVICE_ID_SELECTED -ge 1 ]]
+  then
+    # get service name
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+    SERVICE_NAME_FORMATTED=$(echo $SERVICE_NAME | sed -r 's/^ *| *$//g')
+    # get customer info
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # if customer doesn't exist
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      # get new customer name
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+
+      # insert new customer
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+    fi
+
+    CUSTOMER_NAME_FORMATTED=$(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')
+
+    # get time
+    echo -e "\nWhat time would you like your $SERVICE_NAME_FORMATTED, $CUSTOMER_NAME_FORMATTED"
+    read SERVICE_TIME
+
+    # get customer_id
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # insert appointment
+    INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+    echo -e "\nI have put you down for a $SERVICE_NAME_FORMATTED at $SERVICE_TIME, $CUSTOMER_NAME_FORMATTED."
+  else
+    # send to main menu
+    MAIN_MENU "I could not find that service. What would you like today?"
+  fi
+}
+
+MAIN_MENU
